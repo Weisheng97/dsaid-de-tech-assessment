59b2f1334a05b08a96c43d025a0803de946a5990
Weisheng97 <52645050+Weisheng97@users.noreply.github.com>
2022-07-10 20:15:51 +0800

    Added transform_data dag and necessary functions
    Added all functions needed to transform raw dataset and save the newly transformed dataset into the processed folder. the raw datasets will be removed from raw folder once it is being processed

--- docker/airflow/dags/daily_dag.py
+++ docker/airflow/dags/daily_dag.py
@@ -12,9 +12,6 @@ import os
 import glob
 
 
-# DAG directory path
-dag_path = os.getcwd()
-
 #Function takes in a dataframe and return a transformed dataframe
 def process_files(df):
 
@@ -41,29 +38,61 @@ def process_files(df):
     
     return new_df
 
+def transform_data():
+    #change the raw_path according to your own raw folder path
+    # raw_path = 'C:\\Users\\User\\OneDrive\\Desktop\\dsaid-de-tech-assessment\\docker\\airflow\\raw_data'
+    raw_path = r'C:\Users\User\OneDrive\Desktop\dsaid-de-tech-assessment\docker\airflow\raw_data'
+
+    #change the raw_path according to your own processed folder path    
+    # processed_path = 'C:\\Users\\User\\OneDrive\\Desktop\\dsaid-de-tech-assessment\\docker\\airflow\\processed_data'
+    processed_path = r'C:\Users\User\OneDrive\Desktop\dsaid-de-tech-assessment\docker\airflow\processed_data'
+
+    # use glob to get all the csv files in the raw folder
+    csv_files = glob.glob(os.path.join(raw_path, "*.csv"))
+    
+    # loop over the list of csv files
+    for f in csv_files:
+
+        # read the csv file
+        df = pd.read_csv(f)
+        # process and transform file
+        df = process_files(df)
+
+        #rename the file name by adding processed infront
+        file_name = "processed_"+ f.split("\\")[-1]
+
+        # save processed dataset in processed folder
+        df.to_csv(f"{processed_path}/{file_name}", index=False)
+
+        #remove raw dataset file once processed
+        os.remove(f)
+
 
 # Define arguments for the DAG
 default_args = {
     "owner": "airflow",
-    "depends_on_past": False,
-    "email": ["fxk_9370@hotmail.com"],  # Can change to the user of choice
-    "email_on_failure": True,
-    "email_on_retry": True,
-    "retries": 2,
-    "retry_delay": timedelta(minutes=10),
-    "start_date": datetime(2022, 7, 1),  # Change this date when first initialised
+    # "depends_on_past": False,
+    # "email": ["fxk_9370@hotmail.com"],  # Can change to the user of choice
+    # "email_on_failure": True,
+    # "email_on_retry": True,
+    # "retries": 2,
+    # "retry_delay": timedelta(minutes=10),
+    "start_date": days_ago(2),  # Change this date when first initialised
 }
 
 ingestion_dag = DAG(
-    'booking_ingestion',
+    'ingestion_transform_dataset',
     default_args=default_args,
     description='Process and transform raw csv',
-    schedule_interval=timedelta(days=1),
+    # schedule_interval=timedelta(days=1),
+    schedule_interval='0 1 * * *',    #to start DAG daily on 1am 
     catchup=False
 )
 
 task_1 = PythonOperator(
     task_id='transform_data',
-    python_callable=transform_data,
+    python_callable= transform_data,
     dag=ingestion_dag,
 )
+
+task_1
